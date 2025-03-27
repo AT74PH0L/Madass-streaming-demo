e4a120a4e8848d419b2e41768d50f8ce75336362
Sunwatcha303 <watcharakorn@gmail.com>
2025-03-28 03:01:57 +0700

    add admin all movies page

---
+++ frontend/src/components/AdminMovies/type.ts
@@ -0,0 +1,6 @@
+export interface Movie {
+  id: string;
+  name: string;
+  pathImg: string;
+  description: string;
+}
---
+++ frontend/src/page/AdminMoviesPage.tsx
@@ -0,0 +1,131 @@
+import { authenApi } from "@/api/authen";
+import { Movie } from "@/components/AdminMovies/type";
+import { DeleteMovieDialog } from "@/components/studio/DeleteMovieDialog";
+import { EditMovieDialog } from "@/components/studio/EditMovieDialog";
+import { MovieCard } from "@/components/studio/MovieCard";
+import { Button } from "@/components/ui/button";
+import axiosInstance from "@/utils/axios";
+import { Plus } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+export default function AdminDashboardage() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [isCreating, setIsCreating] = useState(false);
+  const navigate = useNavigate();
+
+  const handleEdit = (movie: Movie) => {
+    setSelectedMovie(movie);
+    setIsCreating(false);
+    setIsEditDialogOpen(true);
+  };
+
+  const handleDelete = (movie: Movie) => {
+    setSelectedMovie(movie);
+    setIsDeleteDialogOpen(true);
+  };
+
+  const handleCreate = () => {
+    setSelectedMovie(null);
+    setIsCreating(true);
+    setIsEditDialogOpen(true);
+  };
+
+  const handleSaveMovie = () => {
+    setIsEditDialogOpen(false);
+  };
+
+  const handleDeleteConfirm = async () => {
+    try {
+      console.log(selectedMovie?.id);
+      await axiosInstance.delete(`/movies/${selectedMovie?.id}`);
+      toast.success("Delete movie successfully");
+    } catch (error) {
+      toast.error("Failed to delete movie");
+      console.error(error);
+    }
+    if (selectedMovie) {
+      setMovies(movies.filter((movie) => movie.id !== selectedMovie.id));
+      setIsDeleteDialogOpen(false);
+    }
+  };
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await authenApi();
+        if (response.role != "admin") {
+          navigate("/home");
+        }
+      } catch (error) {
+        console.error("Error fetching authentication:", error);
+        navigate("/home");
+      }
+    };
+
+    fetchData();
+  }, [navigate]);
+  
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axiosInstance.get("/movies");
+        if (response.data) {
+          setMovies(response.data);
+        } else {
+          console.error("Failed to fetch movies:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+    fetchMovies();
+  }, []);
+  return (
+    <>
+      <div className="">
+        <div className="flex justify-between items-center mb-8 ml-10 mr-10">
+          <h1
+            className="text-3xl font-bold cursor-pointer"
+            onClick={() => navigate("/admin")}
+          >
+            Back office
+          </h1>
+          <Button onClick={handleCreate}>
+            <Plus className="mr-2 h-4 w-4" /> Add Movie
+          </Button>
+        </div>
+        <div className="flex items-center justify-center text-3xl font-bold text-orange-300">
+          {movies.length === 0 ? <div>No movies available</div> : null}
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-8 max-w-6xl mx-auto">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+              onEdit={() => handleEdit(movie)}
+              onDelete={() => handleDelete(movie)}
+            />
+          ))}
+        </div>
+        <EditMovieDialog
+          open={isEditDialogOpen}
+          onOpenChange={setIsEditDialogOpen}
+          movie={selectedMovie}
+          isCreating={isCreating}
+          onSave={handleSaveMovie}
+        />
+        <DeleteMovieDialog
+          open={isDeleteDialogOpen}
+          onOpenChange={setIsDeleteDialogOpen}
+          movieTitle={selectedMovie?.name || ""}
+          onConfirm={handleDeleteConfirm}
+        />
+      </div>
+    </>
+  );
+}
