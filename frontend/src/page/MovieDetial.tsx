aa8f3f27e9e12310c8222124e818e43c4f12f136
Sunwatcha303 <watcharakorn@gmail.com>
2025-03-29 02:12:38 +0700

    update movie detaile page

--- frontend/src/page/MovieDetial.tsx
+++ frontend/src/page/MovieDetial.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axiosInstant from "@/utils/axios";
 import { Button } from "@/components/ui/button";
+import { CirclePlayIcon } from "lucide-react";
 
 interface Movie {
   id: string;
@@ -35,15 +36,27 @@ const MovieDetail: React.FC = () => {
   }
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>{movie.name}</h1>
-      <img
-        src={movie.pathImg}
-        alt={movie.description}
-        style={{ width: "300px" }}
-      />
-      <p>{movie.description}</p>
-      <Button onClick={() => navigate(`/review/${id}`)}>Review</Button>
+    <div className="w-2/3 mx-auto grid gap-2">
+      <div className="flex flex-col items-center mb-6">
+        <h1 className="text-5xl font-bold mb-4">{movie.name}</h1>
+        <p>{movie.description}</p>
+      </div>
+      <div className="aspect-video rounded-lg mb-8 flex items-center justify-center relative">
+        <div
+          className="absolute inset-0 opacity-40"
+          style={{
+            backgroundImage: `url(${movie.pathImg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div>
+        <p className="relative z-10">
+          <CirclePlayIcon className="logo w-16 h-16 cursor-pointer hover:scale-110 active:scale-95 transition-transform duration-200" />
+        </p>
+      </div>
+
+      <Button onClick={() => navigate(`/review/${id}`)} className="cursor-pointer">Review</Button>
     </div>
   );
 };
